827a01341f0aa940b7d4bbc964bcf4a451ddbe08
Merlin Harold <merlinharold@gmail.coml>
2020-08-20 13:33:58 -0500

    El código inicial de viverro_uno

---
+++ vivero_uno.R
@@ -0,0 +1,13 @@
+
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+
+#cargamos los datos
+plants <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/plants.csv')
+actions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/actions.csv')
+threats <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-08-18/threats.csv')
+
+
+
+
